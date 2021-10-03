fefd24929de474e7b96a84772b3aec4d589c35c0
FrederickMih <fredtimermih@gmail.com>
2021-10-03 05:34:47 +0100

    Install and imported bootstrap

--- src/actions/login.js
+++ src/actions/login.js
@@ -1,1 +1,12 @@
-import { LOGIN } from "./constants";
+import { LOGIN } from "./constants"
+
+const loginAction = (username) => {
+  dispatch({
+    type: LOGIN,
+    payload: {
+      username,
+      loggedIn: true,
+    },
+  })
+}
+export default loginAction;
