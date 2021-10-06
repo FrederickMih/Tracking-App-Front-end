6e23cc0ce6e4bf42e26e53532b9a671df24288d6
FrederickMih <fredtimermih@gmail.com>
2021-10-06 19:20:25 +0100

    Render the Navigation component in Router

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Navigator from '../components/container/Navigator';
 
 const Router = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route />
+      </Switch>
+      <Navigator />
+    </BrowserRouter>
+  );
+};
 
-export default Router
+export default Router;
