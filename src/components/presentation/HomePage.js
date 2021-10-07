90f604bcf37fecbcf6c18f1744d4bb1fed9892db
FrederickMih <fredtimermih@gmail.com>
2021-10-07 16:35:35 +0100

    Implement the handleToAddMeasure method

--- src/components/presentation/HomePage.js
+++ src/components/presentation/HomePage.js
@@ -1,27 +1,38 @@
 import PropTypes from 'prop-types';
+import { useRef, useState } from 'react';
 import '../../styles/HomePage.css';
 
-const HomePage = ({ handleSlideToAddMeasurement }) => (
-  <div className="main__div">
-    <div className="homeContainer">
-      <p className="homeText">
-        Welcome back let&apos;s add your measurement for today.
-      </p>
-      <span className="reversedTriangle" />
+const HomePage = () => {
+  const containerRef = useRef();
+  const [index, setIndex] = useState(0);
+
+  const handleToAddMeasures = () => {
+    const value = (index + 1) * 100;
+
+    containerRef.current.style.left = `-${value}vw`;
+
+    setIndex((prevIndex) => prevIndex + 1);
+  };
+
+  return (
+    <div className="main__div">
+      <div className="homeContainer">
+        <p className="homeText">
+          Welcome back let&apos;s add your measurement for today.
+        </p>
+        <span className="reversedTriangle" />
+      </div>
+      <div className="buttonBackground">
+        <button type="button" className="button" onClick={handleToAddMeasures}>
+          Go
+        </button>
+      </div>
     </div>
-    <div className="buttonBackground">
-      <button
-        type="button"
-        className="button"
-        onClick={handleSlideToAddMeasurement}>
-        Go
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 HomePage.propTypes = {
-  handleSlideToAddMeasurement: PropTypes.func.isRequired,
+  handleToAddMeasures: PropTypes.func.isRequired,
 };
 
 export default HomePage;
