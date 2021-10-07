079582e19dab580424cc2ecaa003ee9064d02b2b
FrederickMih <fredtimermih@gmail.com>
2021-10-07 10:21:38 +0100

    Modify the Navigator component

--- src/components/container/App.js
+++ src/components/container/App.js
@@ -3,18 +3,22 @@ import { useSelector, useDispatch } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginUser from '../presentation/LoginUser';
 import { getMeasurements } from '../../actions';
+import Router from '../../router/Router';
 // import LogoutUser from '../presentation/LogoutUser';
 import '../../styles/App.css';
 
 const App = () => {
   const dispatch = useDispatch();
+  // get measurements data from the endpoint
   useEffect(() => {
     dispatch(getMeasurements());
   }, [dispatch]);
   // Load user data from state
   const user = useSelector((state) => state.user);
 
-  return <div>{user.loggedIn ? <LogoutUser /> : <LoginUser />}</div>;
+  return (
+    <div>{user.loggedIn ? <LogoutUser /> && <Router /> : <LoginUser />}</div>
+  );
 };
 
 export default App;
