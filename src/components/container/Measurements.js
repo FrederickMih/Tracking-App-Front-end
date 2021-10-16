8b1ac658d0a4125cf21ab65a8b7d6f9c33dc8331
FrederickMih <fredtimermih@gmail.com>
2021-10-16 08:29:03 +0100

    Add some styles for MeasurementItem component

--- src/components/container/Measurements.js
+++ src/components/container/Measurements.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import MeasurementItem from '../presentation/MeasurementItem';
+import '../../styles/Measurements.css';
 
 const Measurements = () => {
   const measurements = useSelector((state) => state.measurements.measurements);
 
   return (
-    <div className="measurements">
+    <div className="measures">
       {measurements.map((measurement) => (
         <MeasurementItem
           key={measurement.id}
