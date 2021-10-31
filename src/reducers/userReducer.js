aae447f7e30d75cce57f1612b75cf292ecb5f1d6
FrederickMih <fredtimermih@gmail.com>
2021-11-01 00:38:14 +0100

    Add userReducer

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,88 @@
+import { setToken } from '../helper/session';
+import {
+  SIGN_UP_USER,
+  LOGIN_USER,
+  USER_REQUEST_FAILURE,
+  LOGOUT_USER,
+  LOGIN_USER_SESSION,
+} from '../actions/constants';
+
+const initialState = {
+  loading: false,
+  loggedIn: false,
+  errorMessage: '',
+  error: '',
+};
+
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SIGN_UP_USER: {
+      const response = action.payload;
+      if (response.auth_token) {
+        setToken(response);
+        return {
+          ...state,
+          loggedIn: true,
+          errorMessage: '',
+          error: '',
+        };
+      }
+      return {
+        ...state,
+        errorMessage: response.message,
+        error: '',
+      };
+    }
+    case USER_REQUEST_FAILURE: {
+      return {
+        ...state,
+        error: action.payload,
+        errorMessage: '',
+      };
+    }
+
+    case LOGIN_USER: {
+      const response = action.payload;
+      if (response.auth_token) {
+        setToken(response);
+        return {
+          ...state,
+          loggedIn: true,
+          error: '',
+          errorMessage: '',
+        };
+      }
+      return {
+        ...state,
+        loading: false,
+        loggedIn: false,
+        error: '',
+        errorMessage: response.message,
+      };
+    }
+
+    case LOGOUT_USER: {
+      setToken({});
+      return {
+        ...state,
+        loggedIn: false,
+        error: '',
+      };
+    }
+
+    case LOGIN_USER_SESSION: {
+      return {
+        ...state,
+        loggedIn: true,
+        error: '',
+      };
+    }
+
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+export default userReducer;
