1e7478d5c7b40175a1343c1569a099e4d0817e52
FrederickMih <fredtimermih@gmail.com>
2021-11-01 01:01:10 +0100

    Add LogInForm.js file

---
+++ src/components/presentation/LogInForm.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { logInUser } from '../../actions/userAction';
+
+const LogInForm = (props) => {
+  const { handleLogIn, error, loggedIn } = props;
+
+  if (loggedIn) {
+    return <Redirect to="/" />;
+  }
+
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUserNameChange = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const details = { username: userName, password };
+    setUserName('');
+    setPassword('');
+    handleLogIn(details);
+  };
+  return (
+    <div className="container w-75 d-flex flex-column justify-content-center align-items-start">
+      <p className="row justify-content-center text-danger">
+        {error !== '' && <span className="d-block m-auto">{error}</span>}
+      </p>
+
+      <form
+        className=" d-flex flex-column align-items-start col-sm-8 col-md-6"
+        onSubmit={handleFormSubmit}>
+        <div className="form-group my-3">
+          <label htmlFor="formGroupExampleInput2">
+            UserName
+            <input
+              type="text"
+              value={userName}
+              className="form-control"
+              placeholder="@username"
+              onChange={handleUserNameChange}
+            />
+          </label>
+        </div>
+        <div className="form-group my-3">
+          <label htmlFor="formGroupExampleInput2">
+            Password
+            <input
+              type="password"
+              value={password}
+              className="form-control"
+              onChange={handlePasswordChange}
+            />
+          </label>
+        </div>
+
+        <button type="submit" className="btn btn-info mb-2">
+          log in
+        </button>
+      </form>
+      <p className="align-self-start">
+        <Link to="/sign_up" style={{ textDecoration: 'none' }}>
+          Create Account
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  handleLogIn: (details) => {
+    dispatch(logInUser(details));
+  },
+});
+
+const mapStateToProps = (state) => ({
+  loggedIn: state.userReducer.loggedIn,
+  error: state.userReducer.error,
+  // errorMessage: state.userReducer.errorMessage,
+});
+
+LogInForm.propTypes = {
+  // errorMessage: PropTypes.string.isRequired,
+  loggedIn: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
+  handleLogIn: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(LogInForm);
