e0300a42b438acdd5565680d505958a0ddeb2aca
FrederickMih <fredtimermih@gmail.com>
2021-11-18 02:19:17 +0100

    Add a snapshots test for BackArrow component

---
+++ src/__tests__/presentation/BackArrow/BackArrow.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import configureAppStore from '../../../Redux/configureStore';
+import BackArrow from '../../../components/presentation/BackArrow'
+
+const store = configureAppStore();
+
+describe('BackArrow', () => {
+  test('should match the snapshot of BackArrow', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <BrowserRouter>
+            <BackArrow title="bkbar" />
+          </BrowserRouter>
+        </Provider>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
