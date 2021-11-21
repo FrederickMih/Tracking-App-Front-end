1a748b3246b94388f7004a83d1073c2dbf0d34c0
FrederickMih <fredtimermih@gmail.com>
2021-11-21 04:18:47 +0100

    Add username in the Morepage component

--- src/components/container/MorePage.js
+++ src/components/container/MorePage.js
@@ -1,3 +1,5 @@
+import { Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Header from './Header';
 import target from '../../assets/images/target.png';
 import email from '../../assets/images/email.png';
@@ -5,40 +7,46 @@ import man from '../../assets/images/man.png';
 import settings from '../../assets/images/settings.png';
 import help from '../../assets/images/help.png';
 import { container } from '../../styles/allPages.module.css';
-import { user, info, link } from '../../styles/MorePage.module.css';
+import { user, link, info } from '../../styles/MorePage.module.css';
 import Profile from './Profile';
+import { getUserInfo } from '../../Redux/slicers/user';
 
-const MorePage = () => (
-  <div>
-    <Header title="More" />
-    <Profile />
-    <div className={container}>
-      <div className={user}>
-        <img alt="Img" src={man} />
-        <div className={info} />
-      </div>
-      <div className={link}>
-        <img alt="target" src={target} />
-        <div>Your goal</div>
-      </div>
-      <div className={link}>
-        <img alt="target" src={email} />
-        <div>Mailing list</div>
-      </div>
-      <div className={link}>
-        <img alt="target" src={man} />
-        <div>Your profile</div>
-      </div>
-      <div className={link}>
-        <img alt="target" src={settings} />
-        <div>Settings</div>
-      </div>
-      <div className={link}>
-        <img alt="target" src={help} />
-        <div>Help</div>
+const MorePage = () => {
+  const userInfo = useSelector(getUserInfo);
+
+  if (userInfo.logged_in === false) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div>
+      <Header title="More" />
+      <Profile />
+      <div className={container}>
+        <div className={user}>
+          <img alt="Img" src={man} />
+        </div>
+        <div className={info}>
+          <h2>{userInfo.username}</h2>
+        </div>
+        <div className={link}>
+          <img alt="target" src={target} />
+          <div>Your goal</div>
+        </div>
+        <div className={link}>
+          <img alt="target" src={email} />
+          <div>Mailing list</div>
+        </div>
+        <div className={link}>
+          <img alt="target" src={settings} />
+          <div>Settings</div>
+        </div>
+        <div className={link}>
+          <img alt="target" src={help} />
+          <div>Help</div>
+        </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default MorePage;
